e91ebb19ceff36e68c3e475b777c003150cdfe8b
Dima <directordmh@tut.by>
2019-11-11 09:42:02 +0300

    v.1.03

---
+++ precache-manifest.ff1a751023e2926a25e689416d2b775d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1befb0ec66060be6d0b332c87f9dafd",
+    "url": "/test-exporer/index.html"
+  },
+  {
+    "revision": "8ffced328188ae837e13",
+    "url": "/test-exporer/static/css/3.86c32f4f.chunk.css"
+  },
+  {
+    "revision": "2edd3d6eb0e956361528",
+    "url": "/test-exporer/static/css/main.67c4a8d4.chunk.css"
+  },
+  {
+    "revision": "0e256625626a9381778a",
+    "url": "/test-exporer/static/js/2.10a93ecf.chunk.js"
+  },
+  {
+    "revision": "8ffced328188ae837e13",
+    "url": "/test-exporer/static/js/3.69d6c038.chunk.js"
+  },
+  {
+    "revision": "2edd3d6eb0e956361528",
+    "url": "/test-exporer/static/js/main.0057d666.chunk.js"
+  },
+  {
+    "revision": "7c0cb5078d4e60cb208d",
+    "url": "/test-exporer/static/js/runtime-main.6762c51a.js"
+  }
+]);
